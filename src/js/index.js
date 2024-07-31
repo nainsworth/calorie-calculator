557486a51a74c3925015abbf33e0edbb07aa28e0
nainsworth <nja8161995@gmail.com>
2024-07-30 20:44:05 -0500

    Made some styling changes and got radio settings to work

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,28 @@
 import "../assets/styles/main.scss";
 
+const imperial = document.querySelector("#imperial");
+const metric = document.querySelector("#metric");
+const imperialForm = document.querySelector("#imperial-from");
+const metricForm = document.querySelector("#metric-form");
+
+const units = document.querySelectorAll("input[name=radio-units]");
+units.forEach((unit) =>
+  unit.addEventListener("change", function () {
+    checkUnit();
+  })
+);
+
+const checkUnit = () => {
+  if (imperial.checked) {
+    document.querySelector(".imperial-form").hidden = false;
+    document.querySelector(".metric-form").hidden = true;
+  } else if (metric.checked) {
+    document.querySelector(".imperial-form").hidden = true;
+    document.querySelector(".metric-form").hidden = false;
+  }
+};
+
+
 // const formToObj = (form) => {
 //   Object.fromEntries(new FormData(form));
 // };
