a90ff4c1635b53533b659cab06a0155c5b4ac764
nainsworth <nja8161995@gmail.com>
2024-08-01 11:58:09 -0500

    Began styling changes and removed metric system

--- src/js/index.js
+++ src/js/index.js
@@ -1,26 +1,24 @@
 import "../assets/styles/main.scss";
 
 const imperial = document.querySelector("#imperial");
-const metric = document.querySelector("#metric");
 const imperialForm = document.querySelector("#imperial-from");
-const metricForm = document.querySelector("#metric-form");
 
-const units = document.querySelectorAll("input[name=radio-units]");
-units.forEach((unit) =>
-  unit.addEventListener("change", function () {
-    checkUnit();
-  })
-);
+// const units = document.querySelectorAll("input[name=radio-units]");
+// units.forEach((unit) =>
+//   unit.addEventListener("change", function () {
+//     checkUnit();
+//   })
+// );
 
-const checkUnit = () => {
-  if (imperial.checked) {
-    document.querySelector(".imperial-form").hidden = false;
-    document.querySelector(".metric-form").hidden = true;
-  } else if (metric.checked) {
-    document.querySelector(".imperial-form").hidden = true;
-    document.querySelector(".metric-form").hidden = false;
-  }
-};
+// const checkUnit = () => {
+//   if (imperial.checked) {
+//     document.querySelector(".imperial-form").hidden = false;
+//     document.querySelector(".metric-form").hidden = true;
+//   } else if (metric.checked) {
+//     document.querySelector(".imperial-form").hidden = true;
+//     document.querySelector(".metric-form").hidden = false;
+//   }
+// };
 
 
 // const formToObj = (form) => {
