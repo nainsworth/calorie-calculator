4247de7513405b1a12ebf169c60e24069989d220
nainsworth <nja8161995@gmail.com>
2024-08-11 19:58:46 -0500

    Updated everything into a sing obj and everything calculates properly

--- dist/index.bundle.js
+++ dist/index.bundle.js
@@ -7,17 +7,35 @@
   \***************************/
 /***/ (() => {
 
+var calorieTarget = document.getElementById("calorie-target");
+var proteinTarget = document.getElementById("protein-target");
+var fatTarget = document.getElementById("fat-target");
+var carbTarget = document.getElementById("carb-target");
+var fiberTarget = document.getElementById("fiber-target");
 var proteinRatio = document.getElementById("protein-ratio");
 var carbRatio = document.getElementById("carb-ratio");
 var fatRatio = document.getElementById("fat-ratio");
+var macroSliders = document.querySelectorAll('input[type="range"]');
+var proteinSlider = document.getElementById("carb-ratio-slider");
 var carbSlider = document.getElementById("carb-ratio-slider");
 var fatSlider = document.getElementById("fat-ratio-slider");
-var macroSliders = document.querySelectorAll('input[type="range"]');
-var ratioArray = [];
+var calorieObj = {
+  calories: 2000,
+  ratio: {
+    protein: 30,
+    fat: 30,
+    carbs: 40
+  },
+  macro: {
+    protein: 150,
+    fat: 67,
+    carbs: 200,
+    fiber: 28
+  }
+};
 
 // Macro Range Slider Event Listener
 macroSliders.forEach(function (slider) {
-  ratioArray.push(slider.value);
   slider.oninput = function (e) {
     return updateMacroInfo(e);
   };
@@ -26,25 +44,42 @@ macroSliders.forEach(function (slider) {
 // Updates Macro Info
 var updateMacroInfo = function updateMacroInfo(e) {
   if (e.target.name == "protein-ratio-slider") {
-    ratioArray[0] = e.target.value;
-    ratioArray[2] = 100 - ratioArray[0] - ratioArray[1];
-    proteinRatio.innerText = "".concat(ratioArray[0], "%");
-    carbRatio.innerText = "".concat(ratioArray[2], "%");
-    carbSlider.value = ratioArray[2];
+    calorieObj.ratio.protein = e.target.value;
+    calorieObj.ratio.carbs = 100 - calorieObj.ratio.protein - calorieObj.ratio.fat;
+    updateTargets();
+    loadTargets();
   } else if (e.target.name == "fat-ratio-slider") {
-    ratioArray[1] = e.target.value;
-    ratioArray[2] = 100 - ratioArray[0] - ratioArray[1];
-    fatRatio.innerText = "".concat(ratioArray[1], "%");
-    carbRatio.innerText = "".concat(ratioArray[2], "%");
-    carbSlider.value = ratioArray[2];
+    calorieObj.ratio.fat = e.target.value;
+    calorieObj.ratio.carbs = 100 - calorieObj.ratio.protein - calorieObj.ratio.fat;
+    updateTargets();
+    loadTargets();
   } else if (e.target.name == "carb-ratio-slider") {
-    ratioArray[2] = e.target.value;
-    ratioArray[1] = 100 - ratioArray[0] - ratioArray[2];
-    carbRatio.innerText = "".concat(ratioArray[2], "%");
-    fatRatio.innerText = "".concat(ratioArray[1], "%");
-    fatSlider.value = ratioArray[1];
+    calorieObj.ratio.carbs = e.target.value;
+    calorieObj.ratio.fat = 100 - calorieObj.ratio.protein - calorieObj.ratio.carbs;
+    updateTargets();
+    loadTargets();
   }
 };
+
+// Update Protein Target
+var updateTargets = function updateTargets() {
+  calorieObj.macro.protein = Math.trunc(calorieObj.calories * (calorieObj.ratio.protein * 0.01) / 4);
+  calorieObj.macro.fat = Math.trunc(calorieObj.calories * (calorieObj.ratio.fat * 0.01) / 9);
+  calorieObj.macro.carbs = Math.trunc(calorieObj.calories * (calorieObj.ratio.carbs * 0.01) / 4);
+};
+var loadTargets = function loadTargets() {
+  proteinTarget.innerText = "".concat(calorieObj.macro.protein, "g");
+  fatTarget.innerText = "".concat(calorieObj.macro.fat, "g");
+  carbTarget.innerText = "".concat(calorieObj.macro.carbs, "g");
+  calorieTarget.innerText = calorieObj.calories;
+  proteinRatio.innerText = "".concat(calorieObj.ratio.protein, "%");
+  fatRatio.innerText = "".concat(calorieObj.ratio.fat, "%");
+  carbRatio.innerText = "".concat(calorieObj.ratio.carbs, "%");
+  proteinSlider.value = calorieObj.ratio.protein;
+  fatSlider.value = calorieObj.ratio.fat;
+  carbSlider.value = calorieObj.ratio.carbs;
+};
+loadTargets();
 
 /***/ }),
 
--- src/js/results.js
+++ src/js/results.js
@@ -9,55 +9,50 @@ const carbRatio = document.getElementById("carb-ratio");
 const fatRatio = document.getElementById("fat-ratio");
 
 const macroSliders = document.querySelectorAll('input[type="range"]');
+const proteinSlider = document.getElementById("carb-ratio-slider");
 const carbSlider = document.getElementById("carb-ratio-slider");
 const fatSlider = document.getElementById("fat-ratio-slider");
 
-let ratioArray = [];
-
-let macroObj = {
+let calorieObj = {
   calories: 2000,
-  protein: 150,
-  fats: 67,
-  carbs: 200,
-  fiber: 28,
+  ratio: {
+    protein: 30,
+    fat: 30,
+    carbs: 40,
+  },
+  macro: {
+    protein: 150,
+    fat: 67,
+    carbs: 200,
+    fiber: 28,
+  },
 };
 
 // Macro Range Slider Event Listener
 macroSliders.forEach((slider) => {
-  ratioArray.push(slider.value);
-
   slider.oninput = (e) => updateMacroInfo(e);
 });
 
 // Updates Macro Info
 const updateMacroInfo = (e) => {
   if (e.target.name == "protein-ratio-slider") {
-    ratioArray[0] = e.target.value;
-    ratioArray[2] = 100 - ratioArray[0] - ratioArray[1];
+    calorieObj.ratio.protein = e.target.value;
+    calorieObj.ratio.carbs =
+      100 - calorieObj.ratio.protein - calorieObj.ratio.fat;
 
-    proteinRatio.innerText = `${ratioArray[0]}%`;
-    carbRatio.innerText = `${ratioArray[2]}%`;
-    carbSlider.value = ratioArray[2];
-    
     updateTargets();
     loadTargets();
   } else if (e.target.name == "fat-ratio-slider") {
-    ratioArray[1] = e.target.value;
-    ratioArray[2] = 100 - ratioArray[0] - ratioArray[1];
-
-    fatRatio.innerText = `${ratioArray[1]}%`;
-    carbRatio.innerText = `${ratioArray[2]}%`;
-    carbSlider.value = ratioArray[2];
+    calorieObj.ratio.fat = e.target.value;
+    calorieObj.ratio.carbs =
+      100 - calorieObj.ratio.protein - calorieObj.ratio.fat;
 
     updateTargets();
     loadTargets();
   } else if (e.target.name == "carb-ratio-slider") {
-    ratioArray[2] = e.target.value;
-    ratioArray[1] = 100 - ratioArray[0] - ratioArray[2];
-
-    carbRatio.innerText = `${ratioArray[2]}%`;
-    fatRatio.innerText = `${ratioArray[1]}%`;
-    fatSlider.value = ratioArray[1];
+    calorieObj.ratio.carbs = e.target.value;
+    calorieObj.ratio.fat =
+      100 - calorieObj.ratio.protein - calorieObj.ratio.carbs;
 
     updateTargets();
     loadTargets();
@@ -66,17 +61,28 @@ const updateMacroInfo = (e) => {
 
 // Update Protein Target
 const updateTargets = () => {
-  macroObj.protein = Math.trunc((macroObj.calories * (ratioArray[0] * 0.01)) / 4);
-  macroObj.fats = Math.trunc((macroObj.calories * (ratioArray[1] * 0.01)) / 9);
-  macroObj.carbs = Math.trunc((macroObj.calories * (ratioArray[2] * 0.01)) / 4);
-  // macroObj.calories = macroObj.protein * 4 + macroObj.fats * 9 + macroObj.carbs * 4;
+  calorieObj.macro.protein = Math.trunc((calorieObj.calories * (calorieObj.ratio.protein * 0.01)) / 4);
+  calorieObj.macro.fat = Math.trunc(
+    (calorieObj.calories * (calorieObj.ratio.fat * 0.01)) / 9
+  );
+  calorieObj.macro.carbs = Math.trunc(
+    (calorieObj.calories * (calorieObj.ratio.carbs * 0.01)) / 4
+  );
 };
 
 const loadTargets = () => {
-  proteinTarget.innerText = `${macroObj.protein}g`;
-  carbTarget.innerText = `${macroObj.carbs}g`;
-  fatTarget.innerText = `${macroObj.fats}g`;
-  calorieTarget.innerText = macroObj.calories;
+  proteinTarget.innerText = `${calorieObj.macro.protein}g`;
+  fatTarget.innerText = `${calorieObj.macro.fat}g`;
+  carbTarget.innerText = `${calorieObj.macro.carbs}g`;
+  calorieTarget.innerText = calorieObj.calories;
+
+  proteinRatio.innerText = `${calorieObj.ratio.protein}%`;
+  fatRatio.innerText = `${calorieObj.ratio.fat}%`;
+  carbRatio.innerText = `${calorieObj.ratio.carbs}%`;
+
+  proteinSlider.value = calorieObj.ratio.protein;
+  fatSlider.value = calorieObj.ratio.fat;
+  carbSlider.value = calorieObj.ratio.carbs;
 };
 
 loadTargets();
